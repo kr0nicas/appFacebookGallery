51c4aa57a28953f2eb2ee49e81626c275eccd823
Jorge Ochoa <ochoa.j@hotmail.com>
2013-12-18 21:08:28 -0600

    Cambio en titulo de la pagina
    Cambio en el titulo del index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <html>
 	<head>
-		<title>Image Uploader</title>
+		<title>Gallery of images</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
